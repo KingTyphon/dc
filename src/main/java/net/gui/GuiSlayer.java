1e5b5c4c80c0153c8cf31c78ade3f885da1ae083
KingTyphon <76860028+KingTyphon@users.noreply.github.com>
2023-09-22 17:59:43 -0400

    Update

--- src/main/java/net/gui/GuiSlayer.java
+++ src/main/java/net/gui/GuiSlayer.java
@@ -3,10 +3,12 @@ package net.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.networking.messages.tech.Tech;
 import net.networking.Networking;
+import net.networking.messages.tech.Tech;
 import net.util.capabilities.slayer.SlayerProvider;
 import net.util.capabilities.techniquecapability.TechProvider;
 import net.util.handlers.Reference;
@@ -24,14 +26,13 @@ public class GuiSlayer extends GuiScreen {
     int guiWidth = 200;
     int guiHeight = 178;
 
-    UpgradeButton sword;
-    GuiButton button1;
     EntityPlayer player = Minecraft.getMinecraft().player;
 
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partials) {
 
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         drawDefaultBackground();
         Minecraft.getMinecraft().renderEngine.bindTexture(texture);
 
@@ -53,13 +54,13 @@ public class GuiSlayer extends GuiScreen {
 
         //Displays the users stats
         drawHoveringText("Slayer Info", centerX+100,centerY+10);
-        fontRenderer.drawString("Health: " + health, centerX+10, centerY+20, 0);
-        fontRenderer.drawString("Speed: " + speed, centerX+10, centerY+40, 0);
-        if(player.hasCapability(SlayerProvider.Breath_CAP, null))fontRenderer.drawString("Breath: " + breath, centerX+10, centerY+60, 0);
-        else fontRenderer.drawString("Mana: " + breath, centerX+10, centerY+60, 0);
+        fontRenderer.drawString("Health: " + health, centerX+10, centerY+5, 0);
+        fontRenderer.drawString("Speed: " + speed, centerX+10, centerY+30, 0);
+        if(player.hasCapability(SlayerProvider.Breath_CAP, null))fontRenderer.drawString("Breath: " + breath, centerX+10, centerY+55, 0);
+        else fontRenderer.drawString("Mana: " + breath, centerX+10, centerY+55, 0);
         fontRenderer.drawString("Strength: "+ strength, centerX+10, centerY+80, 0);
-        fontRenderer.drawString("Durability: ", centerX+10, centerY+100, 0);
-        fontRenderer.drawString("Technique: "+ technique, centerX+10, centerY+120, 0);
+        fontRenderer.drawString("Durability: ", centerX+10, centerY+105, 0);
+        fontRenderer.drawString("Technique: "+ technique, centerX+10, centerY+130, 0);
         fontRenderer.drawString("Xp: " + Xp , centerX+10, centerY+160, 0);
         fontRenderer.drawString("SP: " + sp, centerX+55, centerY+145, 0);
         fontRenderer.drawString("Rank: ", centerX+ 110, centerY+ 20, 0);
@@ -67,13 +68,7 @@ public class GuiSlayer extends GuiScreen {
         fontRenderer.drawString(mana +"/" + maxMana, centerX + 110, centerY +60 ,0);
         fontRenderer.drawString("Lvl: " + level, centerX+ 10, centerY+145, 0);
 
-        //Bird mouse hover overlay
-        if(mouseX >= centerX+166 && mouseX <= centerX+ 206 && mouseY >= centerY-32 && mouseY <= centerY){
-            List<String> text = new ArrayList<String>();
-            text.add("CAW CAW CA-New Mission: Join The Discord!");
-            text.add("CAW CAW CAW");
-            drawHoveringText(text,mouseX, mouseY);
-        }
+
         int breathNum = player.getCapability(SlayerProvider.Breath_CAP, null).getBreath();
 
         switch(breathNum){
@@ -125,6 +120,17 @@ public class GuiSlayer extends GuiScreen {
                 fontRenderer.drawString("Stone Breathing", centerX +110, centerY +50, 11448243);
                 break;
         }
+        super.drawScreen(mouseX, mouseY, partials);
+        if(mouseX >= centerX+166 && mouseX <= centerX+ 206 && mouseY >= centerY-32 && mouseY <= centerY){
+            List<String> text = new ArrayList<String>();
+            text.add("CAW CAW CA-New Mission: Join The Discord!");
+            text.add("CAW CAW CAW");
+            drawHoveringText(text,mouseX, mouseY);
+        }
+    }
+    @Override
+    protected void renderToolTip(ItemStack stack, int x, int y){
+
     }
 
     @Override
@@ -132,12 +138,12 @@ public class GuiSlayer extends GuiScreen {
         int centerX = (width / 2) - guiWidth / 2;
         int centerY = (height / 2) - guiHeight / 2;
         this.buttonList.clear();
-        this.addButton(new UpgradeButton(6, centerX,centerY+120, ""));
-        this.addButton(new UpgradeButton(5, centerX,centerY+100, ""));
-        this.addButton(new UpgradeButton(4, centerX,centerY+80, ""));
-        this.addButton(new UpgradeButton(3, centerX,centerY+60, ""));
-        this.addButton(new UpgradeButton(2, centerX,centerY+40, ""));
-        this.addButton(new UpgradeButton(1, centerX,centerY+20, ""));
+        this.addButton(new UpgradeButton(6, centerX-20,centerY+125, ""));
+        this.addButton(new UpgradeButton(5, centerX-20,centerY+100, ""));
+        this.addButton(new UpgradeButton(4, centerX-20,centerY+75, ""));
+        this.addButton(new UpgradeButton(3, centerX-20,centerY+50, ""));
+        this.addButton(new UpgradeButton(2, centerX-20,centerY+25, ""));
+        this.addButton(new UpgradeButton(1, centerX-20,centerY, ""));
         this.addButton(new GuiButton(0, centerX+guiWidth-82, centerY+guiHeight-20, 75, 20, "Skill Tree"));
         super.initGui();
     }
@@ -146,7 +152,6 @@ public class GuiSlayer extends GuiScreen {
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
         //checks for the buttons
-        if(player.getCapability(TechProvider.TECH_CAP,null).getSp() >0){
         switch (button.id){
             case 0:
                 Minecraft.getMinecraft().displayGuiScreen(new SkillTreeGui());
@@ -190,7 +195,6 @@ public class GuiSlayer extends GuiScreen {
                 break;
             default:
                 break;
-        }
         }
 
         super.actionPerformed(button);
